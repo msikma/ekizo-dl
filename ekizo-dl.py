7a22cb7dec24c2b7ed73cc88f18e943179b5bf46
Michiel Sikma <michiel@sikma.org>
2020-02-21 18:23:09 +0100

    Add searches for shop items

--- ekizo-dl.py
+++ ekizo-dl.py
@@ -7,6 +7,7 @@ import subprocess
 import urllib.request
 import time
 import json
+import html as htmlmodule
 import urllib.parse
 from pprint import pprint
 
@@ -15,8 +16,16 @@ def mandarake_search_url(search):
   return 'https://ekizo.mandarake.co.jp/auction/item/itemsListJa.html?t=0&q={}'.format(urllib.parse.quote(search))
 
 
+def mandarake_shop_search_url(search):
+  return 'https://order.mandarake.co.jp/order/listPage/list?categoryCode={}&keyword={}'.format(search[1], urllib.parse.quote(search[0]))
+
+
 def get_url_base(url):
   return '/'.join(url.split('/')[:-1]) + '/'
+
+
+def get_shop_url_base(url):
+  return '/'.join(url.split('/')[:3]) + '/'
 
 
 def load_json(file):
@@ -45,6 +54,19 @@ def get_cache():
     return load_json(cache)
   else:
     return {}
+
+
+def fetch_shop_images(html, base):
+  images_re = re.compile(r"<img\s+src\s*=\s*\"(https://img.mandarake.co.jp/webshopimg/)(.+?)s_(.+?)\".+?>")
+  images = [(m.group(1) + m.group(2) + m.group(3)).strip() for m in images_re.finditer(html)]
+  return images
+
+
+def fetch_shop_links(html, base):
+  links_re = re.compile(r"<a\s+href\s*=\s*\"(/order/detailPage/item)(.+?)\".*>")
+  links_path = list(set([(m.group(1) + m.group(2)).strip() for m in links_re.finditer(html)]))
+  links = list(map(lambda x: urllib.parse.unquote(htmlmodule.unescape(base.rstrip('/') + x)), links_path))
+  return links
 
 
 def fetch_links(html, base):
@@ -102,11 +124,18 @@ def get_url_id(url):
   return id
 
 
+def get_shop_url_id(url):
+  id = re.search(r'itemCode=(.+?)&', url)
+  id = id.group(1) if id else None
+  return id
+
+
 def download_url(url, search, id, target):
+  search = search[0] if isinstance(search, list) else search
   search_dir = target + '/' + search
   ensure_dir(search_dir)
   os.chdir(search_dir)
-  proc = subprocess.run(['ascr', url])
+  proc = subprocess.run(['ascr', '--dir-min', '1', '--overwrite', url])
   return proc.returncode == 0
 
 
@@ -116,6 +145,29 @@ def main():
 
   target = search_data['target']
   ensure_dir(target)
+
+  for search in search_data['searches_shop']:
+    time.sleep(1)
+    url = mandarake_shop_search_url(search)
+    base = get_shop_url_base(url)
+    html = urllib.request.urlopen(url).read().decode('utf-8')
+    urls = fetch_shop_links(html, base)
+    utilized_urls = 0
+
+    for url in urls:
+      time.sleep(1)
+      # Check if we've already downloaded this link before.
+      has_cache = check_cache(cache, url)
+      if has_cache: continue
+      id = get_shop_url_id(url)
+      result = download_url(url, search, id, target)
+      report_dl(url, search, id, result)
+      #cache = add_to_cache(cache, url, search)
+      if not result: continue
+      utilized_urls += 1
+    
+    report_search(url, search, urls, utilized_urls)
+
   for search in search_data['searches']:
     time.sleep(5)
     url = mandarake_search_url(search)
